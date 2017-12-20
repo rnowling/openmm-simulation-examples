be0aeff59ec081d08251c3098f9aaf186cc66b7d
RJ Nowling <rnowling@gmail.com>
2017-12-20 17:43:58 -0600

    Rdl sim 1 should use a 2 fs timestep

--- run_rdl2_sim_1.sh
+++ run_rdl2_sim_1.sh
@@ -5,9 +5,9 @@ python amber_gbsa.py \
        --positions-fl simulations/rdl2_implicit_sim_01.dcd \
        --energies-fl simulations/rdl2_implicit_sim_01.energies.tsv \
        --output-state simulations/rdl2_impilict_sim_01.state \
-       --steps-output 10000 \
+       --steps-output 5000 \
        --temperature 300.0 \
        --damping 50.0 \
-       --timestep 0.001 \
+       --timestep 0.002 \
        --forcefield amber99sbildn \
        --pdb-file models/model1_fixed_moved2_eq1.pdb
