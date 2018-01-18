dfef43d67ddeb99e3783d5ca4139b94e6acea919
RJ Nowling <rnowling@gmail.com>
2018-01-17 23:34:05 -0600

    print eq distribution

--- analysis-scripts/pca.py
+++ analysis-scripts/pca.py
@@ -332,10 +332,11 @@ def calculate_transition_matrix(args):
     u, v = LA.eigh(transitions)
 
     u = u[::-1]
+    v = v[:, ::-1]
     timescales = - args.timestep * lag_time / np.log(u[1:])
-    
-    print timescales
 
+    print "timescales", timescales
+    print "eq dist", v[0]
     
 def parseargs():
     parser = argparse.ArgumentParser()
