216a6cc10fbc1efc53f4073ee73f3999cb07866d
RJ Nowling <rnowling@gmail.com>
2018-01-17 22:47:59 -0600

    Fix return parameters

--- analysis-scripts/pca.py
+++ analysis-scripts/pca.py
@@ -280,9 +280,9 @@ def sweep_clusters(args):
     inertia_values = []
     for k in args.n_clusters:
         print "Clustering with %s states" % k
-        _, _, inertia, _ = k_means(projected[:, args.dimensions],
-                                   k,
-                                   n_jobs=-2)
+        _, _, inertia = k_means(projected[:, args.dimensions],
+                                k,
+                                n_jobs=-2)
         inertia_values.append(inertia)
 
     plt.plot(args.n_clusters,
