0d31dda7324e6f6ea633db707861e1dd34414db8
RJ Nowling <rnowling@gmail.com>
2018-01-18 16:00:04 -0600

    print labels of first/last 10 frames

--- analysis-scripts/pca.py
+++ analysis-scripts/pca.py
@@ -318,8 +318,8 @@ def calculate_transition_matrix(args):
                                  tol=0.00001,
                                  n_init=25)
 
-    print labels[10:]
     print labels[:10]
+    print labels[-10:]
     
     pop_counts = np.zeros(args.n_clusters,
                           dtype=np.int)
