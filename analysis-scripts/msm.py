c212bf10b9411b442f723e0bd4c5e05a1be1f6fa
RJ Nowling <rnowling@gmail.com>
2018-02-13 22:33:56 -0600

    use 1-based indexing for chi

--- analysis-scripts/msm.py
+++ analysis-scripts/msm.py
@@ -355,7 +355,8 @@ def compare_dihedral_distributions(args):
         
         # not all residues have chi dihedrals
         top = traj.topology
-        resids = [top.atom(atoms[0]).index for atoms in atom_indices]
+        # convert to 1-based indexing
+        resids = [top.atom(atoms[0]).index + 1 for atoms in atom_indices]
 
     for state_1 in xrange(msm.n_states - 1):
         state_1_frames = [idx for idx, state in enumerate(msm.labels) \
